302c0479029c1ad21b647ef266fbd0be1579ff48
Harsh joon <harshjoon222@gmail.com>
2023-12-15 09:50:25 +0530

    socket

---
+++ Python/Libraries/socket/intro.py
@@ -0,0 +1,7 @@
+'''
+
+BERKELEY SOCKETS - is an API for internet sockets and UNIX domain sockets, used for inter-process communications.
+A socket is an abstract representation for the local endpoint of a network communication path.
+
+
+'''  
